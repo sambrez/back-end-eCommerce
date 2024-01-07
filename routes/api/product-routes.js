472b5c697c0e8afb18d604b660836ec71071660f
Sam Brez <sambreslin02@gmail.com>
2024-01-07 18:18:18 -0500

    updates to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,14 +9,8 @@ router.get('/', (req, res) => {
   // be sure to include its associated Category and Tag data
   Product.findAll({
     include: [
-      {
-        model: Category,
-        attributes: ['id', 'category_name']
-      },
-      {
-        model: Tag,
-        attributes: ['id', 'tag_name'],
-      }
+      { model: Category },
+      { model: Tag, through: ProductTag }
     ]
   })
     .then(products => res.json(products))
@@ -31,10 +25,10 @@ router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   Product.findOne({
-    where: {id: req.params.id},
+    where: { id: req.params.id },
     include: [
-      {model: Category},
-      {model: Tag},
+      { model: Category },
+      { model: Tag, through: ProductTag },
     ]
   })
     .then(singleProduct => {
@@ -60,10 +54,16 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -71,47 +71,46 @@ router.post('/', (req, res) => {
           };
         });
         return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
+      } 
+        // if no product tags, just respond
+        res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+      res.status(400).json({ message: 'TEST' });
     });
 });
 
 // update product
 router.put('/:id', (req, res) => {
   // update product data
- Product.update(req.body, {
+  Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-        
+
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
           // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
-          .filter((tag_id) => !productTagIds.includes(tag_id))
-          .map((tag_id) => {
-            return {
-              product_id: req.params.id,
-              tag_id,
-            };
-          });
+            .filter((tag_id) => !productTagIds.includes(tag_id))
+            .map((tag_id) => {
+              return {
+                product_id: req.params.id,
+                tag_id,
+              };
+            });
 
-            // figure out which ones to remove
+          // figure out which ones to remove
           const productTagsToRemove = productTags
-          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-          .map(({ id }) => id);
-                  // run both actions
+            .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+            .map(({ id }) => id);
+          // run both actions
           return Promise.all([
             ProductTag.destroy({ where: { id: productTagsToRemove } }),
             ProductTag.bulkCreate(newProductTags),
@@ -130,11 +129,11 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
   Product.destroy({
-    where: {id: req.params.id}
+    where: { id: req.params.id }
   })
     .then(deletedProduct => {
       if (!deletedProduct) {
-        res.status(404).json({ message: 'Product not found'});
+        res.status(404).json({ message: 'Product not found' });
         return;
       }
       res.json(deletedProduct);
